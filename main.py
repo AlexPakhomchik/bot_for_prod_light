7f834845d7a41f773f56ca14034ab148d4dab02e
AlexPakhomchik <korsar7772@gmail.com>
2023-01-22 14:36:16 +0300

    add back-button for all menu

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import json
-
 import requests
 import telebot
 from dotenv import load_dotenv
 import os
-
+from telebot import types
 from get_material import *
 
 load_dotenv()
@@ -22,6 +20,11 @@ def start_message(message):
     bot.send_message(message.chat.id, "Welcome! Press the 'Menu' button to see options.", reply_markup=markup)
 
 
+@bot.message_handler(func=lambda message: message.text == 'Назад в меню')
+def handle_menu_button(message):
+    start_message(message)
+
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text == 'Добавить материал':
@@ -31,7 +34,8 @@ def handle_text(message):
         button_3 = telebot.types.KeyboardButton('Драйвера')
         button_4 = telebot.types.KeyboardButton('Крышки')
         button_5 = telebot.types.KeyboardButton('Система крепления')
-        markup.add(button_1, button_2, button_3, button_4, button_5)
+        button_6 = types.KeyboardButton('Назад в меню')
+        markup.add(button_1, button_2, button_3, button_4, button_5, button_6)
         bot.send_message(message.chat.id,
                          "Menu: \n1. Option 1 \n2. Option 2 \n3. Option 3 \n4. Option 4 \n5. Option 5",
                          reply_markup=markup)
@@ -43,7 +47,8 @@ def handle_text(message):
         button_3 = telebot.types.KeyboardButton('Драйвера')
         button_4 = telebot.types.KeyboardButton('Крышки')
         button_5 = telebot.types.KeyboardButton('Система крепления')
-        markup.add(button_1, button_2, button_3, button_4, button_5)
+        button_6 = types.KeyboardButton('Назад в меню')
+        markup.add(button_1, button_2, button_3, button_4, button_5, button_6)
         bot.send_message(message.chat.id,
                          "Menu: \n1. Option 1 \n2. Option 2 \n3. Option 3 \n4. Option 4 \n5. Option 5",
                          reply_markup=markup)
@@ -55,28 +60,29 @@ def handle_text(message):
         button_3 = telebot.types.KeyboardButton('Драйвера')
         button_4 = telebot.types.KeyboardButton('Крышки')
         button_5 = telebot.types.KeyboardButton('Система крепления')
-        markup.add(button_1, button_2, button_3, button_4, button_5)
+        button_6 = types.KeyboardButton('Назад в меню')
+        markup.add(button_1, button_2, button_3, button_4, button_5, button_6)
         bot.send_message(message.chat.id,
                 'Menu: \n1. Профиль \n2. Светодиодные модули 2 \n3. Драйвера \n4. Крышки \n5. Система крепления',
                 reply_markup=markup)
     elif message.text == 'Профиль':
-        response = requests.get('http://127.0.0.1:9000/profile/')
+        response = requests.get('http://127.0.0.1:9000/api/profile/')
         data = response.json()
         bot.send_message(message.chat.id, get_all_profile(data))
     elif message.text == 'Светодиодные модули':
-        response = requests.get('http://127.0.0.1:9000/module/')
+        response = requests.get('http://127.0.0.1:9000/api/module/')
         data = response.json()
         bot.send_message(message.chat.id, get_all_module(data))
     elif message.text == 'Драйвера':
-        response = requests.get('http://127.0.0.1:9000/driver/')
+        response = requests.get('http://127.0.0.1:9000/api/driver/')
         data = response.json()
         bot.send_message(message.chat.id, get_all_drivers(data))
     elif message.text == 'Крышки':
-        response = requests.get('http://127.0.0.1:9000/cover/')
+        response = requests.get('http://127.0.0.1:9000/api/cover/')
         data = response.json()
         bot.send_message(message.chat.id, get_all_covers(data))
     elif message.text == 'Система крепления':
-        response = requests.get('http://127.0.0.1:9000/mounting_system/')
+        response = requests.get('http://127.0.0.1:9000/api/mounting_system/')
         data = response.json()
         bot.send_message(message.chat.id, get_all_mounting_system(data))
 
