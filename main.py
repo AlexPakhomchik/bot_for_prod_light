b2eded7b47e9aaa07df748109eaba66a51ba19ae
AlexPakhomchik <korsar7772@gmail.com>
2023-02-14 18:49:54 +0300

    restructure code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 from telebot import types
 from get_material import *
-from materials import get_number_of_materials, MATERIALS, DEL_MATERIALS, ADD_MATERIALS, CREATE_MATERIALS
+from materials import *
 
 load_dotenv()
 
@@ -20,8 +20,6 @@ def start_message(message):
     Parameters:
     message (telebot.types.Message): A Message object that represents the incoming message from the user.
     """
-    id_user = message.from_user.id
-    print(id_user)
     markup = telebot.types.ReplyKeyboardMarkup()
     markup.row(telebot.types.KeyboardButton('Добавить материал'))
     markup.row(telebot.types.KeyboardButton('Списать материал'))
@@ -37,74 +35,6 @@ def handle_menu_button(message):
 The main menu is displayed by calling the start_message function.
     """
     start_message(message)
-
-
-def add_material_get_name_material(message, chapter):
-    """
-    This function takes two parameters as input:
-    - message (object): The message object received from the chat API.
-    - chapter (object): The chapter object associated with the material being added.
-
-    The function prompts the user for the name of the material to be added. Once the name is received, the function calls
-    the add_material_get_value_material function to prompt the user for the quantity of the material.
-    """
-    data = message.text.upper()
-    inp = bot.send_message(message.chat.id, 'Теперь количество')
-    bot.register_next_step_handler(inp, add_material_get_value_material, data, chapter)
-
-
-def add_material_get_value_material(message, data, chapter):
-    """
-    This function takes three parameters as input:
-    - message (object): The message object received from the chat API.
-    - data (str): The name of the material to be added.
-    - chapter (object): The chapter object associated with the material being added.
-
-    The function retrieves the current number of materials for the specified chapter. It then makes a GET request to an API
-    endpoint to retrieve the current value of the material. The function adds the newly provided quantity to the current value
-    and updates the material count by making a PUT request to the API endpoint. Finally, the function sends a confirmation
-    message to the user indicating that the material has been added.
-    """
-    number_materials = get_number_of_materials(chapter)
-    url = f'http://127.0.0.1:9000/api/{chapter}/{number_materials.get(data)}/'
-    previous_value_request = requests.get(url)
-    last_value_json = previous_value_request.json()
-    last_value = last_value_json['value']
-    end_data = {chapter: data, 'value': int(message.text)}
-    end_data['value'] = last_value + end_data['value']
-    response = requests.put(url, data=end_data)
-    bot.send_message(message.chat.id, f"Материал добавлен")
-
-
-def del_material_get_name_material(message, chapter):
-    data = message.text.upper()
-    inp = bot.send_message(message.chat.id, 'Теперь количество')
-    bot.register_next_step_handler(inp, del_material_get_value_material, data, chapter)
-
-
-def del_material_get_value_material(message, data, chapter):
-    number_materials = get_number_of_materials(chapter)
-    url = f'http://127.0.0.1:9000/api/{chapter}/{number_materials.get(data)}/'
-    previous_value_request = requests.get(url)
-    last_value_json = previous_value_request.json()
-    last_value = last_value_json['value']
-    end_data = {chapter: data, 'value': int(message.text)}
-    end_data['value'] = last_value - end_data['value']
-    response = requests.put(url, data=end_data)
-    bot.send_message(message.chat.id, f"Материал списан")
-
-
-def create_material_get_name_material(message, chapter):
-    data = message.text.upper()
-    inp = bot.send_message(message.chat.id, 'Теперь количество')
-    bot.register_next_step_handler(inp, create_material_get_value_material, data, chapter)
-
-
-def create_material_get_value_material(message, data, chapter):
-    url = f'http://127.0.0.1:9000/api/{chapter}/'
-    end_data = {chapter: data, 'value': int(message.text)}
-    response = requests.post(url, data=end_data)
-    bot.send_message(message.chat.id, f"Материал создан")
 
 
 @bot.message_handler(content_types=['text'])
@@ -128,7 +58,7 @@ def handle_text(message):
         bot.send_message(message.chat.id, "Пожалуйста, введите "
                                         "наименование и количество, которое хотите добавить")
         bot.send_message(message.chat.id, get_all_materials(data, ADD_MATERIALS[message.text]))
-        bot.register_next_step_handler(message, add_material_get_name_material, chapter)
+        bot.register_next_step_handler(message, add_material_get_name_material, chapter, bot)
 
 
     if message.text == 'Списать материал':
@@ -141,7 +71,7 @@ def handle_text(message):
         button_6 = types.KeyboardButton('Назад в меню')
         markup.add(button_1, button_2, button_3, button_4, button_5, button_6)
         bot.send_message(message.chat.id,
-                         "Menu: \n1. Option 1 \n2. Option 2 \n3. Option 3 \n4. Option 4 \n5. Option 5",
+                         "Выберите категорию",
                          reply_markup=markup)
     elif message.text in list(DEL_MATERIALS.keys()):
         chapter = DEL_MATERIALS[message.text]
@@ -150,7 +80,7 @@ def handle_text(message):
         bot.send_message(message.chat.id, "Пожалуйста, введите наименование и количество, которое хотите"
                                           " списать")
         bot.send_message(message.chat.id, get_all_materials(data, DEL_MATERIALS[message.text]))
-        bot.register_next_step_handler(message, del_material_get_name_material, chapter)
+        bot.register_next_step_handler(message, del_material_get_name_material, chapter, bot)
 
 
 
@@ -191,8 +121,7 @@ def handle_text(message):
         chapter = CREATE_MATERIALS[message.text]
         bot.send_message(message.chat.id, "Пожалуйста, введите "
                                         "наименование материала, которое хотите создать")
-        bot.register_next_step_handler(message, create_material_get_name_material, chapter)
+        bot.register_next_step_handler(message, create_material_get_name_material, chapter, bot)
 
-
-
-bot.polling()
+if __name__ == "__main__":
+    bot.polling()
--- materials.py
+++ materials.py
@@ -1,5 +1,7 @@
 import requests
 
+from main import bot
+
 MATERIALS = {'Профиль': 'profile', 'Светодиодные модули': 'module', 'Драйвера': 'driver', 'Крышки': 'cover',
              'Система крепления': 'mounting_system'}
 
@@ -28,3 +30,72 @@ def get_number_of_materials(material):
         dict_of_material[i[material]] = count
         count += 1
     return dict_of_material
+
+
+def add_material_get_name_material(message, chapter, bot):
+    """
+    This function takes two parameters as input:
+    - message (object): The message object received from the chat API.
+    - chapter (object): The chapter object associated with the material being added.
+
+    The function prompts the user for the name of the material to be added. Once the name is received, the function calls
+    the add_material_get_value_material function to prompt the user for the quantity of the material.
+    """
+    data = message.text.upper()
+    inp = bot.send_message(message.chat.id, 'Теперь количество')
+    bot.register_next_step_handler(inp, add_material_get_value_material, data, chapter)
+
+
+def add_material_get_value_material(message, data, chapter):
+    """
+    This function takes three parameters as input:
+    - message (object): The message object received from the chat API.
+    - data (str): The name of the material to be added.
+    - chapter (object): The chapter object associated with the material being added.
+
+    The function retrieves the current number of materials for the specified chapter. It then makes a GET request to an API
+    endpoint to retrieve the current value of the material. The function adds the newly provided quantity to the current value
+    and updates the material count by making a PUT request to the API endpoint. Finally, the function sends a confirmation
+    message to the user indicating that the material has been added.
+    """
+    print("Adding")
+    number_materials = get_number_of_materials(chapter)
+    url = f'http://127.0.0.1:9000/api/{chapter}/{number_materials.get(data)}/'
+    previous_value_request = requests.get(url)
+    last_value_json = previous_value_request.json()
+    last_value = last_value_json['value']
+    end_data = {chapter: data, 'value': int(message.text)}
+    end_data['value'] = last_value + end_data['value']
+    response = requests.put(url, data=end_data)
+    bot.send_message(message.chat.id, f"Материал добавлен")
+
+
+def del_material_get_name_material(message, chapter, bot):
+    data = message.text.upper()
+    inp = bot.send_message(message.chat.id, 'Теперь количество')
+    bot.register_next_step_handler(inp, del_material_get_value_material, data, chapter)
+
+
+def del_material_get_value_material(message, data, chapter):
+    number_materials = get_number_of_materials(chapter)
+    url = f'http://127.0.0.1:9000/api/{chapter}/{number_materials.get(data)}/'
+    previous_value_request = requests.get(url)
+    last_value_json = previous_value_request.json()
+    last_value = last_value_json['value']
+    end_data = {chapter: data, 'value': int(message.text)}
+    end_data['value'] = last_value - end_data['value']
+    response = requests.put(url, data=end_data)
+    bot.send_message(message.chat.id, f"Материал списан")
+
+
+def create_material_get_name_material(message, chapter, bot):
+    data = message.text.upper()
+    inp = bot.send_message(message.chat.id, 'Теперь количество')
+    bot.register_next_step_handler(inp, create_material_get_value_material, data, chapter)
+
+
+def create_material_get_value_material(message, data, chapter):
+    url = f'http://127.0.0.1:9000/api/{chapter}/'
+    end_data = {chapter: data, 'value': int(message.text)}
+    response = requests.post(url, data=end_data)
+    bot.send_message(message.chat.id, f"Материал создан")
