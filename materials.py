68fd03d2c3a3db5ae2613992ef7e6845ee9baf3f
AlexPakhomchik <korsar7772@gmail.com>
2023-02-14 20:45:07 +0300

    rebuild func get_number_of_materials

--- materials.py
+++ materials.py
@@ -25,11 +25,7 @@ def get_number_of_materials(material):
     """
     response = requests.get(f'http://127.0.0.1:9000/api/{material}/')
     data = response.json()
-    count = 1
-    dict_of_material = {}
-    for i in data:
-        dict_of_material[i[material]] = count
-        count += 1
+    dict_of_material = {item['profile']: item['id'] for item in data}
     return dict_of_material
 
 
